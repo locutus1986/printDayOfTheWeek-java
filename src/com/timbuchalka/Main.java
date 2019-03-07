79ed653490a143bf0ca448791ce2080b62e068b6
locutus1986 <locutus1986@localhost.localdomain>
2019-03-07 10:37:38 -0500

    first

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,46 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    private static void printDayOfTheWeek(){
+        int day = 1;
+
+        switch(day){
+            case 0:
+                System.out.println("Sunday");
+                break;
+
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            case 4:
+                System.out.println("Thursday");
+                break;
+
+            case 5:
+                System.out.println("Friday");
+                break;
+
+            case 6:
+                System.out.println("Saturday");
+                break;
+
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+    }
+}
